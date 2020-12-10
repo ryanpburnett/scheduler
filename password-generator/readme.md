2ce73b8bbbf5199eb19aa45c76d732cda01878ef
Ryan <ryanpburnett@yahoo.com>
2020-12-10 13:36:43 -0600

    first commit

---
+++ password-generator/readme.md
@@ -0,0 +1,18 @@
+# README
+
+# Password Generator
+
+This is a site for generating random passwords at a 10 character length.  There are four degrees of security:
+
+* 1 --- only lower case
+* 2 --- lower and upper case
+* 3 --- lower, upper and numbers
+* 4 --- lower, upper, numbers and special characters
+
+## Usage
+
+A button will prompt you to generate a password.  Upon pushing it, you will recieve a popup asking if you want to have a 1 through 4 security level (as described in the previous section).  After selecting a level, a passwork will be generated in the box on the page.  
+
+## Questions?
+
+Email Ryan at ryanpburnett@yahoo.com
---
+++ password-generator/script.js
@@ -0,0 +1,59 @@
+function pass1(length) {
+  var result           = '';
+  var characters       = 'abcdefghijklmnopqrstuvwxyz';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function pass2(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function pass3(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function pass4(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function popup() {
+  var userChoice = prompt("Security level of password (1 = all lowercase, 2 = mixed upper and lower case, 3 = added numbers, 4 = added special characters")
+  
+    
+  if 
+    (userChoice === "1"){
+    document.getElementById("password").innerHTML = pass1(10)
+  }else if
+    (userChoice === "2"){
+    document.getElementById("password").innerHTML = pass2(10)
+  }else if
+    (userChoice === "3"){
+    document.getElementById("password").innerHTML = pass3(10)
+  }else{
+    document.getElementById("password").innerHTML = pass4(10)
+  }
+}
+
+document.getElementById("generate").addEventListener("click", popup);
